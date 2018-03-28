d7315df98174745f3c3a9bd2f07d928e46107e6b
Oscar Trollhag <oscar.trollhag@businessandemotions.se>
2018-03-29 00:33:22 +0200

    Finished migrating from plugin style

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
-import Lemonade from './admin.js'
-import App from './App'
-import Start from '@/views/start'
+import AdminApp from './AdminApp'
+import AdminMenu from '@/components/admin/admin-menu'
+import RouteMenu from '@/components/admin/route-menu'
+
+import Install from '@/views/admin/install'
+import Login from '@/views/login'
+import Dashboard from '@/views/admin/dashboard'
 
 import Title from '@/components/core/title.js';
 import '../lemonade/styles/main.scss';
@@ -15,9 +19,9 @@ import '../lemonade/styles/main.scss';
 Vue.use(VueRouter)
 Vue.use(VueResource)
 
-Vue.component('App', App)
+Vue.component('AdminApp', AdminApp)
+Vue.component('login', Login)
 Vue.component('the-title', Title)
-Vue.component('Start', Start)
 
 if (!window.routes) {
   window.routes = []
@@ -25,17 +29,45 @@ if (!window.routes) {
 
 Vue.config.productionTip = false
 
-Vue.use(Lemonade, {
-  init: (App, routes) => {
-    var router = new VueRouter({
-      routes: routes
-    })
-
-    new Vue({
-      el: '#app',
-      router,
-      components: { App },
-      template: '<App/>'
-    })
+window.EventBus = new Vue({
+  data() {
+    return {
+      bodyClass: []
+    }
+  },
+  watch: {
+    bodyClass(val) {
+      document.getElementsByTagName('body')[0].className = val;
+    } 
   }
 })
+
+const init = function() {
+  window.routes.forEach((route) => {
+    route.component = Vue.component(route.component)
+    if (route.children) {
+      route.children.forEach((child) => {
+        child.component = Vue.component(child.component)
+      })
+    }
+  })
+  var router = new VueRouter({
+    routes: window.routes
+  })
+  
+  new Vue({
+    el: '#app',
+    router,
+    components: { AdminApp },
+    template: '<AdminApp/>'
+  })
+}
+Vue.http.get('/api/admin/').then((response) => {
+  if (response.body === true) {
+    Vue.component('Install', Install)
+    Vue.component('admin-menu', AdminMenu)
+    Vue.component('route-menu', RouteMenu)
+    Vue.component('dashboard', Dashboard)
+  }
+  init()
+}, () => { init() })
