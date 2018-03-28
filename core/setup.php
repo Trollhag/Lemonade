ff912085eee6984b399d2c9f4a5a9fe35de3c9c4
Oscar Trollhag <oscar.trollhag@businessandemotions.se>
2018-03-29 00:26:31 +0200

    Dev stuff in setup

--- core/setup.php
+++ core/setup.php
@@ -1,5 +1,15 @@
 <?php
 namespace Lemonade\Setup;
+use Lemonade\Router\Router;
+use Lemonade\Options\Options;
 
-$Lemon->router->register("/", "Install", [ "installing" => true ]);
-$Lemon->assets->enqueue("routes/js", "inline_js", "var routes = " . json_encode($Lemon->router->getRoutes()));
+Options::autoload();
+
+if (!defined("LEMONADE_I")) {
+    Router::register("/", "Install", [ "installing" => true ]);
+}
+$Lemon->assets->enqueue("routes/js", "inline_js", "var routes = " . json_encode(Router::getRoutes()));
+$Lemon->assets->enqueue('lemon/js', 'inline_js', "var lemon = " . json_encode([
+    "status" => $Lemon->status,
+    "options" => Options::_toFront()
+]));
