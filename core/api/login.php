dc57c21b604a95fe352395a7c9454a9f19c04752
Oscar Trollhag <oscar.trollhag@businessandemotions.se>
2018-03-29 00:28:05 +0200

    API: Finished login route

--- core/api/login.php
+++ core/api/login.php
@@ -1,18 +1,32 @@
 <?php
-if (!defined('ABSPATH')) {
+use Lemonade\Users\User;
+
+if (!defined('LEMONADE_V')) {
     die(-1);
 }
 
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $current = User::currentUser();
+    if ($current !== false) {
+        http_response_code(400);
+        die('Allready logged in');
+    }
 
-$username = 'username';
-$password = 'password';
- 
-$random1 = 'secret_key1';
-$random2 = 'secret_key2';
- 
-$hash = md5($random1.$pass.$random2); 
- 
-$self = $_SERVER['REQUEST_URI'];
+    $username = Lemonade\sanitize_post_input('username', ['is_string'], ['trim']);
+    $password = Lemonade\sanitize_post_input('password', ['is_string']);
+
+    if (!User::validate($username) && !filter_var($username, FILTER_VALIDATE_EMAIL)) {
+        http_response_code(400);
+        die("Invalid username or email");
+    }
+    $valid = User::login($username, $password);
+    if ($valid === false) {
+        http_response_code(400);
+        die("Username and password dont match");
+    }
+    if (is_int($valid) && $valid < 0) {
+        http_response_code(400);
+        die(strval($valid));
+    }
+    die();
+}
